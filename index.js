5369fae515ed6447f38b28ad7b6a94f01f14e75a
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-10-20 15:20:44 +0600

    delete operation is done

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ async function run() {
       res.send(result);
   })
 
+  // delete a item 
+
+  app.delete('/selectedProducts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await selectedProductCollection.deleteOne(query);
+    res.send(result);
+})
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
