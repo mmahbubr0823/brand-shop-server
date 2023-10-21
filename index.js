e65a9478607e7844b0573fa947d5caac80e46e56
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-10-21 10:30:42 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors())
 app.use(express.json())
-//PeHaD1Bqtnna4Qzd
-// foodAndBeverage
 
-
-const uri = "mongodb+srv://foodAndBeverage:PeHaD1Bqtnna4Qzd@cluster0.scdnbhm.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scdnbhm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
